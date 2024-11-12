c75fa52b0f802303d979ce79393e82160c39121e
samuelhasiholan <samuelhasiholanjsc@gmail.com>
2024-11-13 03:05:55 +0700

    update

--- components/main/section-fitur.tsx
+++ components/main/section-fitur.tsx
@@ -1,85 +1,81 @@
-import { Button } from '@nextui-org/button'
-import { Image } from '@nextui-org/image'
+import { Button } from "@nextui-org/button";
+import { Image } from "@nextui-org/image";
 
 interface SectionFiturProps {
-    openModal: (title: string, iframeUrl: string) => void
+  openModal: (title: string, iframeUrl: string) => void;
 }
 
 const SectionFitur: React.FC<SectionFiturProps> = (props) => {
-    const data = [
-        {
-            src: 'assets/fitur-1.png',
-            title: 'Monografi Hukum',
-            url:
-                process.env.NEXT_PUBLIC_ACCOUNT_IFRAME_URL +
-                '/webview/monografi',
-        },
-        {
-            src: 'assets/fitur-2.png',
-            title: 'Perkara Hukum',
-            url:
-                process.env.NEXT_PUBLIC_ACCOUNT_IFRAME_URL +
-                '/webview/perkara/beranda',
-        },
-        {
-            src: 'assets/fitur-3.png',
-            title: 'Grafik Statistik',
-            url:
-                process.env.NEXT_PUBLIC_ACCOUNT_IFRAME_URL +
-                '/webview/produk_hukum/grafik',
-        },
-        {
-            src: 'assets/fitur-4.png',
-            title: 'Artikel Hukum',
-            url:
-                process.env.NEXT_PUBLIC_ACCOUNT_IFRAME_URL +
-                '/webview/artikel_hukum',
-        },
-        {
-            src: 'assets/fitur-5.png',
-            title: 'Konstitusional Review',
-            url:
-                process.env.NEXT_PUBLIC_ACCOUNT_IFRAME_URL +
-                '/webview/judicial/beranda',
-        },
-        {
-            src: 'assets/fitur-6.png',
-            title: 'Matriks Produk',
-            url:
-                process.env.NEXT_PUBLIC_ACCOUNT_IFRAME_URL +
-                '/webview/produk_hukum/statistik',
-        },
-    ]
+  const data = [
+    {
+      src: "assets/fitur-1.png",
+      title: "Monografi Hukum",
+      url: process.env.NEXT_PUBLIC_ACCOUNT_IFRAME_URL + "/webview/monografi",
+    },
+    {
+      src: "assets/fitur-2.png",
+      title: "Perkara Hukum",
+      url:
+        process.env.NEXT_PUBLIC_ACCOUNT_IFRAME_URL + "/webview/perkara/beranda",
+    },
+    {
+      src: "assets/fitur-3.png",
+      title: "Grafik Statistik",
+      url:
+        process.env.NEXT_PUBLIC_ACCOUNT_IFRAME_URL +
+        "/webview/produk_hukum/grafik",
+    },
+    {
+      src: "assets/fitur-4.png",
+      title: "Artikel Hukum",
+      url:
+        process.env.NEXT_PUBLIC_ACCOUNT_IFRAME_URL + "/webview/artikel_hukum",
+    },
+    {
+      src: "assets/fitur-5.png",
+      title: "Konstitusional Review",
+      url:
+        process.env.NEXT_PUBLIC_ACCOUNT_IFRAME_URL +
+        "/webview/judicial/beranda",
+    },
+    {
+      src: "assets/fitur-6.png",
+      title: "Matriks Produk",
+      url:
+        process.env.NEXT_PUBLIC_ACCOUNT_IFRAME_URL +
+        "/webview/produk_hukum/statistik",
+    },
+  ];
 
-    return (
-        <section className="main-section flex flex-col py-11">
-            {/* <div>
+  return (
+    <section className="main-section flex flex-col py-11">
+      {/* <div>
                 <Button color="primary" disableRipple disableAnimation>FITUR</Button>
             </div> */}
-            <div className="font-bold my-6 text-4xl text-title">
-                Jelajahi Fitur JDIH
-            </div>
-            <div className="grid grid-cols-3 gap-4">
-                {data.map((item, index) => (
-                    <Button
-                        key={index}
-                        className="fitur-card text-xl"
-                        onClick={() => props.openModal(item.title, item.url)}
-                    >
-                        <Image
-                            className="mr-1"
-                            height={60}
-                            alt="pos"
-                            radius="none"
-                            src={item.src}
-                            removeWrapper
-                        />
-                        <span>{item.title}</span>
-                    </Button>
-                ))}
-            </div>
-        </section>
-    )
-}
+      <div className="font-bold mb-6 text-4xl text-title">
+        Jelajahi Fitur JDIH
+      </div>
+      <div className="grid grid-cols-3 gap-4">
+        {data.map((item, index) => (
+          <Button
+            key={index}
+            className="fitur-card text-xl"
+            onClick={() => props.openModal(item.title, item.url)}
+          >
+            <Image
+              className="mr-1"
+              height={60}
+              alt="pos"
+              radius="none"
+              src={item.src}
+              removeWrapper
+            />
+            <span>{item.title}</span>
+          </Button>
+        ))}
+      </div>
+    </section>
+  );
+};
 
-export default SectionFitur
+export default SectionFitur;
--- components/main/section-infografis.tsx
+++ components/main/section-infografis.tsx
@@ -1,129 +1,121 @@
-import { useEffect, useState } from 'react'
-import { Button } from '@nextui-org/button'
-import { Skeleton } from '@nextui-org/skeleton'
-import { InfografisInterface } from '@/app/types/entities'
-import { Image } from '@nextui-org/image'
-import { useHttp } from '@/app/hooks/useHttp'
-import Carousel from 'react-multi-carousel'
-import 'react-multi-carousel/lib/styles.css'
-import moment from 'moment'
+import { useEffect, useState } from "react";
+import { Button } from "@nextui-org/button";
+import { Skeleton } from "@nextui-org/skeleton";
+import { InfografisInterface } from "@/app/types/entities";
+import { Image } from "@nextui-org/image";
+import { useHttp } from "@/app/hooks/useHttp";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import moment from "moment";
 
 export interface SectionInfografisProps {
-    openModal: (type: string, title: string, search?: number | string) => void
+  openModal: (type: string, title: string, search?: number | string) => void;
 }
 
 const SectionInfografis: React.FC<SectionInfografisProps> = (props) => {
-    const { get, isLoading } = useHttp()
-    const [dataTop5, setDataTop5] = useState<InfografisInterface[]>([])
-    const responsive = {
-        any: {
-            breakpoint: { max: 4000, min: 0 },
-            items: 3,
-        },
-    }
+  const { get, isLoading } = useHttp();
+  const [dataTop5, setDataTop5] = useState<InfografisInterface[]>([]);
+  const responsive = {
+    any: {
+      breakpoint: { max: 4000, min: 0 },
+      items: 3,
+    },
+  };
 
-    const top5 = async () => {
-        get('/infografis/top5').then((res: any) => {
-            const data: InfografisInterface[] = []
+  const top5 = async () => {
+    get("/infografis/top5").then((res: any) => {
+      const data: InfografisInterface[] = [];
 
-            if (res?.data && res?.data.length > 0) {
-                res?.data.map((item: any) => {
-                    data.push({
-                        id: item.id,
-                        judul: item.judul,
-                        konten: item.konten,
-                        thumbnail: item.file_path,
-                        dateCreated: item.created_at,
-                    })
-                })
-            }
+      if (res?.data && res?.data.length > 0) {
+        res?.data.map((item: any) => {
+          data.push({
+            id: item.id,
+            judul: item.judul,
+            konten: item.konten,
+            thumbnail: item.file_path,
+            dateCreated: item.created_at,
+          });
+        });
+      }
 
-            setDataTop5(data)
-        })
-    }
+      setDataTop5(data);
+    });
+  };
 
-    useEffect(() => {
-        top5()
-    }, [])
+  useEffect(() => {
+    top5();
+  }, []);
 
-    return (
-        <section className="carousel-section flex flex-col py-11">
-            <div className="secondary-carousel-section">
-                <div className="font-bold my-6 text-4xl text-title">
-                    Infografis
+  return (
+    <section className="carousel-section flex flex-col py-11">
+      <div className="secondary-carousel-section">
+        <div className="font-bold mb-6 text-4xl text-title">Infografis</div>
+        <div className="flex mb-6">
+          <span className="flex-auto text-title"></span>
+          <button
+            className="cursor-pointer flex-shrink-0 font-bold ml-5 text-primary"
+            onClick={() => props.openModal("infografis", "Infografis")}
+          >
+            LIHAT SEMUA
+          </button>
+        </div>
+      </div>
+
+      {isLoading ? (
+        <div className="flex">
+          <Skeleton
+            isLoaded={!isLoading}
+            className="h-24 w-1/3 rounded-lg mx-2"
+          />
+          <Skeleton
+            isLoaded={!isLoading}
+            className="h-24 w-1/3 rounded-lg mx-2"
+          />
+          <Skeleton
+            isLoaded={!isLoading}
+            className="h-24 w-1/3 rounded-lg mx-2"
+          />
+        </div>
+      ) : (
+        dataTop5 && (
+          <Carousel
+            responsive={responsive}
+            arrows={false}
+            infinite
+            autoPlay
+            autoPlaySpeed={8000}
+            itemClass="px-2 pb-3"
+          >
+            <div className="carausel-gap"></div>
+            {dataTop5?.map((value, index) => (
+              <Button
+                className="flex flex-col infografis-card text-small gap-0"
+                key={index}
+                onClick={() =>
+                  props.openModal("infografis", "Infografis", value.id)
+                }
+              >
+                <Image
+                  alt="infografis"
+                  className="object-cover w-full"
+                  src={
+                    process.env.NEXT_PUBLIC_INFOGRAFIS_URL +
+                    "/" +
+                    value.thumbnail
+                  }
+                  radius="none"
+                  removeWrapper
+                />
+                <div className="infografis-card-body">
+                  <p className="font-bold">{value.judul}</p>
                 </div>
-                <div className="flex mb-6">
-                    <span className="flex-auto text-title"></span>
-                    <button
-                        className="cursor-pointer flex-shrink-0 font-bold ml-5 text-primary"
-                        onClick={() =>
-                            props.openModal('infografis', 'Infografis')
-                        }
-                    >
-                        LIHAT SEMUA
-                    </button>
-                </div>
-            </div>
+              </Button>
+            ))}
+          </Carousel>
+        )
+      )}
+    </section>
+  );
+};
 
-            {isLoading ? (
-                <div className="flex">
-                    <Skeleton
-                        isLoaded={!isLoading}
-                        className="h-24 w-1/3 rounded-lg mx-2"
-                    />
-                    <Skeleton
-                        isLoaded={!isLoading}
-                        className="h-24 w-1/3 rounded-lg mx-2"
-                    />
-                    <Skeleton
-                        isLoaded={!isLoading}
-                        className="h-24 w-1/3 rounded-lg mx-2"
-                    />
-                </div>
-            ) : (
-                dataTop5 && (
-                    <Carousel
-                        responsive={responsive}
-                        arrows={false}
-                        infinite
-                        autoPlay
-                        autoPlaySpeed={8000}
-                        itemClass="px-2 pb-3"
-                    >
-                        <div className="carausel-gap"></div>
-                        {dataTop5?.map((value, index) => (
-                            <Button
-                                className="flex flex-col infografis-card text-small gap-0"
-                                key={index}
-                                onClick={() =>
-                                    props.openModal(
-                                        'infografis',
-                                        'Infografis',
-                                        value.id,
-                                    )
-                                }
-                            >
-                                <Image
-                                    alt="infografis"
-                                    className="object-cover w-full"
-                                    src={
-                                        process.env.NEXT_PUBLIC_INFOGRAFIS_URL +
-                                        '/' +
-                                        value.thumbnail
-                                    }
-                                    radius="none"
-                                    removeWrapper
-                                />
-                                <div className="infografis-card-body">
-                                    <p className="font-bold">{value.judul}</p>
-                                </div>
-                            </Button>
-                        ))}
-                    </Carousel>
-                )
-            )}
-        </section>
-    )
-}
-
-export default SectionInfografis
+export default SectionInfografis;
